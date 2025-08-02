429ccda1d20d4470c4edde9e4156441847b432f5
Francis Richard Omeiza <francisrichardomeizae@GMAIL.COM>
2025-08-02 01:44:18 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+num_1 = int(input(">>>"))
+num_2 = int(input(">>>"))
+operation = input("operation")
+if operation == "+":
+    print(
+        num_1 + num_2
+    )
+elif operation == "-":
+    print(
+        num_1 - num_2
+    )
+
+elif operation == "/":
+    print(
+        num_1 / num_2
+    )
+elif operation == "*":
+    print(
+        num_1 * num_2
+    )
