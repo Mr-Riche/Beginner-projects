accfc7e6e06cd9504e729b9415227e7d2c437f6f
Francis Richard Omeiza <francisrichardomeizae@GMAIL.COM>
2025-08-04 23:53:32 +0100

    Update Guessing game (while loop).py
    better version

--- Guessing game (while loop).py
+++ Guessing game (while loop).py
@@ -1,11 +1,11 @@
 print("guess number between 1-10")
 secret_number = 7
-while secret_number > 0:
-    secret_number = int(input(">>>"))
-    if secret_number > 7:
+while True:
+    guess = int(input(">>>"))
+    if guess > secret_number:
         print("too high")
-    if secret_number < 7:
+    if guess < secret_number:
         print("too low")
-    if secret_number == 7:
+    if guess == secret_number:
         print("correct")
         break
